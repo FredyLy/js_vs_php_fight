ae06417c5caaf5cc8f3bd54e4eff59471e2dce65
Fredy <fredy.ly.wcs@gmail.com>
2020-12-25 19:16:50 +0100

    finalizing project

---
+++ src/Vegeta.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import countHits from './countHits';
+import vegeta from './vegeta.png';
+
+class Vegeta extends Component {
+  render() {
+
+    const { name, addOneHit, hocState, life } = this.props;
+
+    const lifeValue = life > 0 ? `${life} %` : <span className="badge badge-danger">K.O.</span>;
+
+    const button = life > 0 
+      ? (<button className="btn btn-success m-3" onClick={addOneHit}>{name} hits!</button>)
+      : (<button className="btn btn-danger m-3" disabled>Beaten!</button>);
+
+    return (
+      <div className="col">
+        
+        <img src={vegeta} alt="vegeta" /><br />
+        { button }
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Hits</th>
+              <th scope="col">Life</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{hocState.hits}</td>
+              <td>{lifeValue}</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+    )
+  }
+}
+
+export default countHits(Vegeta, 10);
